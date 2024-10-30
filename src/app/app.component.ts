e7119f347298a222683285bd58e01ad22ff1352e
debian <debian@localhost.localdomain>
2024-10-30 17:36:05 -0500

    viistas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { GlobalService } from './services/global.service';
 import { SidebarComponent } from "./components/ui/sidebar/sidebar.component";
+import { TopNavbarComponent } from './components/ui/top-navbar/top-navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
     HomeComponent, 
-    SidebarComponent],
+    SidebarComponent,
+  TopNavbarComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
---
+++ src/app/components/ui/top-navbar/top-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { GlobalService } from '../../../services/global.service';
+
+@Component({
+  selector: 'app-top-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-navbar.component.html',
+  styleUrl: './top-navbar.component.css'
+})
+export class TopNavbarComponent {
+  constructor (
+    public global: GlobalService
+  ){}
+}
